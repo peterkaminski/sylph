ab8f1660bce17f37cf4f5c430bd66783d8e7246c
Peter Kaminski <kaminski@istori.com>
2023-03-21 05:51:15 -0700

    first commit

---
+++ sylph.py
@@ -0,0 +1,44 @@
+import openai
+import sys
+import argparse
+
+# Function to call ChatGPT API
+def call_chatgpt(prompt):
+    openai.api_key = "your_api_key_here"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    return response.choices[0].text.strip()
+
+# Function to output to stdout
+def output_stdout(text):
+    print(text)
+
+# Function to output to a file
+def output_file(text, file_path):
+    with open(file_path, "w") as f:
+        f.write(text)
+
+def main():
+    parser = argparse.ArgumentParser(description="Interact with ChatGPT")
+    parser.add_argument("-f", "--file", help="Specify the output file path")
+    args = parser.parse_args()
+
+    user_input = input("Enter your prompt for ChatGPT: ")
+    response = call_chatgpt(user_input)
+
+    if args.file:
+        output_file(response, args.file)
+        print(f"ChatGPT response saved to {args.file}")
+    else:
+        output_stdout(response)
+
+if __name__ == "__main__":
+    main()
